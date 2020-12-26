cd6ad7848747c295fa0714814856aa9fcdd0ea7f
mrwhiterk <mrwhiterk@gmail.com>
2020-12-26 01:33:30 -0500

    41. Adding token service

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,6 +40,7 @@ namespace API.Controllers
 
     [HttpPost("login")]
     public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+      
       var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
       if (user == null) return Unauthorized("Invalid username");
 
