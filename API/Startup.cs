669ffa99de119f26e963eabcdab18dda3d9d91de
mrwhiterk <mrwhiterk@gmail.com>
2021-01-06 01:14:31 -0500

    43. Creating a User DTO and returning the token

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,7 +34,7 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddScoped<ITokenService, TokenService>()
+      services.AddScoped<ITokenService, TokenService>();
       
       services.AddDbContext<DataContext>(options =>
       {
